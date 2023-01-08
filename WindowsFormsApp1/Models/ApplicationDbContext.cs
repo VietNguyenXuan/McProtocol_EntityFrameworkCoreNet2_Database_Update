9c536f40f2ca4786e7d2210e386b141619d05b07
VietNguyenXuan <nguyenviet.06092000@gmail.com>
2023-01-08 15:07:35 +0700

    update first time

---
+++ WindowsFormsApp1/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Models
+{
+  internal class ApplicationDbContext:DbContext
+  {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      var sqlitePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+       @"McProtocol_EntityFrameworkCoreNet2_Database_Update\\database.db");
+
+      if (!Directory.Exists(sqlitePath)) Directory.CreateDirectory(sqlitePath); optionsBuilder.UseSqlite("Data Source=" + sqlitePath + "\\database.db");
+
+    }
+    public DbSet<Student> Students { get; set; }
+  }
+}
